df291872a306bdc3be33647a2d48cbd357497f79
huytehuy <80874663+huytehuy@users.noreply.github.com>
2022-06-15 12:38:18 +0700

    Add files via upload

---
+++ sub_index/config.js
@@ -0,0 +1,17 @@
+const nameGirl = 'Quỳnh Anh';
+const giftUrl = './timeline.html';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng bé nè';
+const contentCard = `
+Qua Tab sau để nghe tui tâm sự nhen<br>
+Tab sau có nhạc nha, nên là mở nhỏ âm lượng nhen<br>
+Nhấp vào cái hình xinh xinh bên phải ó.
+`;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = './sub_index/avatar.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
